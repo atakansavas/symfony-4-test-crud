248df34f2e86292ab397bd32c24cbd3e7e5fff96
atakansavas <a.savas@hotmail.com.tr>
2018-02-22 00:33:43 +0300

    prepare the project.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,8 +10,15 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
 }
